d1268dcbb7fa3073d40cd496944ee013962d9360
chlopez <chlopez@minfin.gob.gt>
2021-11-22 15:13:42 -0600

    Seccion 3 - finalizado v2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
@@ -11,13 +13,14 @@ import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { ProgressComponent } from './pages/progress/progress.component';
 import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { RegisterComponent } from './auth/register/register.component';
 import { PagesComponent } from './pages/pages.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    RegisterComponent,
     NopagefoundComponent,
     DashboardComponent,
     BreadcrumbsComponent,
@@ -25,12 +28,11 @@ import { PagesComponent } from './pages/pages.component';
     HeaderComponent,
     ProgressComponent,
     Grafica1Component,
-    RegisterComponent,
-    PagesComponent
+    PagesComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   bootstrap: [AppComponent]
 })
--- src/app/pages/grafica1/grafica1.component.ts
+++ src/app/pages/grafica1/grafica1.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-grafica1',
   templateUrl: './grafica1.component.html',
-  styles: []
+  styles: [
+  ]
 })
 export class Grafica1Component implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
   }
 
 }
